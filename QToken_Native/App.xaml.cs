002fc94a10b2bf0a4e0a8ba2ce09e8e0625333a0
Prabin Rag TK <prabinrag2340@gmail.com>
2025-09-05 02:12:08 +0400

    Refactor application structure: remove AppShell and MainPage, add LoginPage as main entry point

--- QToken_Native/App.xaml.cs
+++ QToken_Native/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace QToken_Native
+﻿using QToken_Native.Pages;
+
+namespace QToken_Native
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new LoginPage());
         }
     }
 }
---
+++ QToken_Native/Pages/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace QToken_Native.Pages;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
